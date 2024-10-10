261ff5cee71ae573a6aa1c59074a6bc6003e7f3c
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-10-10 20:32:22 +0200

    initial commit

---
+++ src/BetterSoulOrb.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace BetterTotems;
+
+[UsedImplicitly]
+public class BetterSoulOrb : MonoBehaviour
+{
+    public RandomAudioClipTable soulOrbCollectSounds;
+    public ParticleSystem getParticles;
+    public bool awardSoul = true;
+    public bool dontRecycle;
+    private Transform target;
+    private float speed;
+    private float acceleration;
+    private SpriteRenderer sprite;
+    private TrailRenderer trail;
+    private Rigidbody2D body;
+    private AudioSource source;
+    private Coroutine zoomRoutine;
+    public float stretchFactor = 2;
+    public float stretchMinY = 1;
+    public float stretchMaxX = 2;
+    public float scaleModifier;
+    public float scaleModifierMin = 1;
+    public float scaleModifierMax = 1;
+
+    public int SoulGotten = 2;
+
+    public BetterSoulOrb()
+    {
+        awardSoul = true;
+        stretchFactor = 2f;
+        stretchMinY = 1f;
+        stretchMaxX = 2f;
+        scaleModifierMin = 1f;
+        scaleModifierMax = 2f;
+    }
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        trail = GetComponent<TrailRenderer>();
+        body = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        transform.SetPositionZ(UnityEngine.Random.Range(-0.001f, -0.1f));
+        target = HeroController.instance.transform;
+    }
+
+    private void OnDisable()
+    {
+        if (sprite)
+        {
+            sprite.enabled = false;
+        }
+        if (trail)
+        {
+            trail.enabled = false;
+        }
+        if (body)
+        {
+            body.isKinematic = true;
+        }
+        GameManager.instance.UnloadingLevel -= SceneLoading;
+    }
+
+    private void OnEnable()
+    {
+        if (sprite)
+        {
+            sprite.enabled = true;
+        }
+        if (trail)
+        {
+            trail.enabled = true;
+        }
+        if (body)
+        {
+            body.isKinematic = false;
+        }
+        if (zoomRoutine != null)
+        {
+            StopCoroutine(zoomRoutine);
+        }
+        zoomRoutine = null;
+        GameManager.instance.UnloadingLevel += SceneLoading;
+        scaleModifier = UnityEngine.Random.Range(scaleModifierMin, scaleModifierMax);
+    }
+
+    private void Update()
+    {
+        if (body && body.velocity.magnitude < 2.5f && zoomRoutine == null)
+        {
+            zoomRoutine = StartCoroutine(Zoom(true));
+        }
+        FaceAngle();
+        ProjectileSquash();
+    }
+
+    private void SceneLoading()
+    {
+        if (zoomRoutine != null)
+        {
+            StopCoroutine(zoomRoutine);
+        }
+        zoomRoutine = StartCoroutine(Zoom(false));
+    }
+
+    private IEnumerator Zoom(bool doZoom = true)
+    {
+        if (doZoom)
+        {
+            speed = 0f;
+            while (target)
+            {
+                speed += acceleration;
+                speed = Mathf.Clamp(speed, 0f, 30f);
+                acceleration += 0.07f;
+                FireAtTarget();
+                if (Vector2.Distance(target.position, transform.position) < 0.8f)
+                {
+                    break;
+                }
+                yield return null;
+            }
+            Debug.LogError("Soul orb could not get player target!");
+        }
+        body.velocity = Vector2.zero;
+        if (soulOrbCollectSounds)
+        {
+            soulOrbCollectSounds.PlayOneShot(source);
+        }
+        if (getParticles)
+        {
+            getParticles.Play();
+        }
+        if (sprite)
+        {
+            sprite.enabled = false;
+        }
+        if (awardSoul)
+        {
+            HeroController.instance.AddMPCharge(SoulGotten);
+        }
+        SpriteFlash component = HeroController.instance.gameObject.GetComponent<SpriteFlash>();
+        if (component)
+        {
+            component.flashSoulGet();
+        }
+        yield return new WaitForSeconds(0.4f);
+        if (dontRecycle)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.Recycle();
+        }
+        yield break;
+    }
+
+    private void FireAtTarget()
+    {
+        float y = target.position.y - transform.position.y;
+        float x = target.position.x - transform.position.x;
+        float num = Mathf.Atan2(y, x) * 57.295776f;
+        Vector2 velocity;
+        velocity.x = speed * Mathf.Cos(num * 0.017453292f);
+        velocity.y = speed * Mathf.Sin(num * 0.017453292f);
+        body.velocity = velocity;
+    }
+
+    private void FaceAngle()
+    {
+        Vector2 velocity = body.velocity;
+        float z = Mathf.Atan2(velocity.y, velocity.x) * 57.295776f;
+        transform.localEulerAngles = new Vector3(0f, 0f, z);
+    }
+
+    private void ProjectileSquash()
+    {
+        float num = 1f - body.velocity.magnitude * stretchFactor * 0.01f;
+        float num2 = 1f + body.velocity.magnitude * stretchFactor * 0.01f;
+        if (num2 > stretchMaxX)
+        {
+            num2 = stretchMaxX;
+        }
+        if (num < stretchMinY)
+        {
+            num = stretchMinY;
+        }
+        num *= scaleModifier;
+        num2 *= scaleModifier;
+        transform.localScale = new Vector3(num2, num, transform.localScale.z);
+    }
+}
---
+++ src/BetterTotems.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq;
+using System.Reflection;
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using JetBrains.Annotations;
+using UnityEngine;
+using Logger = Modding.Logger;
+
+namespace BetterTotems;
+
+[UsedImplicitly]
+public class BetterTotems : Modding.Mod
+{
+    private static GameObject betterOrb = null;
+
+    public override void Initialize()
+    {
+        BetterLog("!Initialize");
+
+        On.PlayMakerFSM.Start += ChangeTotems;
+
+        //On.GameCameras.Start += AddMasks;
+
+        DoAddMasks(GameCameras.instance);
+
+        BetterLog("~Initialize");
+    }
+
+    private void AddMasks(On.GameCameras.orig_Start orig, GameCameras self)
+    {
+        orig(self);
+    }
+
+    private void DoAddMasks(GameCameras self)
+    {
+        BetterLog("!DoAddMasks");
+        GameObject health1Go = self.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(3).gameObject;
+
+        int totalMaskAmount = 28 * 13;
+        for (int i = 12; i < totalMaskAmount + 1; i++)
+        {
+            GameObject healthGo = Object.Instantiate(health1Go, health1Go.transform.parent);
+            SetHealthPositionAndFsm(healthGo, i);
+        }
+        SetHealthPosition(self.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(4).gameObject, totalMaskAmount + 1);
+        BetterLog("!DoAddMasks");
+    }
+
+    private void SetHealthPositionAndFsm(GameObject ob, int num)
+    {
+        BetterLog("!SetHealthPositionAndFsm");
+        ob.name = $"Health {num}";
+        PlayMakerFSM healthFsm = ob.LocateMyFSM("health_display");
+        FsmVariables healthFsmVars = healthFsm.FsmVariables;
+        healthFsmVars.GetFsmInt("Health Number").Value = num;
+
+        SetHealthPosition(ob, num);
+        BetterLog("!SetHealthPositionAndFsm");
+    }
+
+    private void SetHealthPosition(GameObject ob, int num)
+    {
+        BetterLog("!SetHealthPosition");
+        int tmp = num;
+        int row = 0;
+        while (tmp > 28)
+        {
+            row++;
+            tmp -= 28;
+        }
+        //int row = (int) (((float) num - 1f) / 29f);
+
+        float xPos = -10.32f + (0.94f * (tmp - 1)) + ((row % 2 == 0) ? 0 : (0.94f / 2f));
+        float yPos = 7.7f - (0.8f * row);
+
+        ob.transform.localPosition = new Vector3(xPos, yPos, -2);
+        BetterLog("!SetHealthPosition");
+    }
+
+    private void ChangeTotems(On.PlayMakerFSM.orig_Start orig, PlayMakerFSM self)
+    {
+        orig(self);
+
+        if (self.FsmName != "soul_totem")
+            return;
+
+        BetterLog("!ChangeTotems");
+
+        if (self.FsmStates[0].Fsm == null)
+        {
+            BetterLog("Preprocessing");
+            self.Preprocess();
+        }
+
+        typeof(PlayMakerFSM).GetField("fsmTemplate", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(self, null);
+        FlingObjectsFromGlobalPool fofgpAction = (FlingObjectsFromGlobalPool) self.FsmStates.First(x => x.Name.Equals("Hit")).Actions[8];
+        if (betterOrb == null)
+        {
+            BetterLog("!betterOrb");
+            //betterOrb = new GameObject("Better Soul Orb", typeof(SpriteRenderer), typeof(Rigidbody2D),
+            //    typeof(AudioSource), typeof(BetterSoulOrb));
+            betterOrb = Object.Instantiate(fofgpAction.gameObject.Value);
+            betterOrb.name = "Better Orb";
+            betterOrb.SetActive(false);
+
+            SoulOrb so = betterOrb.GetComponent<SoulOrb>();
+            BetterSoulOrb bso = betterOrb.AddComponent<BetterSoulOrb>();
+
+            bso.soulOrbCollectSounds = so.soulOrbCollectSounds;
+            bso.getParticles = so.getParticles;
+            bso.awardSoul = so.awardSoul;
+            bso.dontRecycle = so.dontRecycle;
+            bso.stretchFactor = so.stretchFactor;
+            bso.stretchMinY = so.stretchMinY;
+            bso.stretchMaxX = so.stretchMaxX;
+            bso.scaleModifier = so.scaleModifier;
+            bso.scaleModifierMin = so.scaleModifierMin;
+            bso.scaleModifierMax = so.scaleModifierMax;
+            bso.SoulGotten = 200;
+
+            Object.DestroyImmediate(betterOrb.GetComponent<SoulOrb>());
+            Object.DontDestroyOnLoad(betterOrb);
+
+            BetterLog("~betterOrb");
+        }
+
+        fofgpAction.gameObject.Value = betterOrb;
+        fofgpAction.spawnMin = 1;
+        fofgpAction.spawnMax = 1;
+
+        BetterLog("~ChangeTotems");
+    }
+
+    private static void BetterLog(string message)
+    {
+        Logger.Log($"[BetterTotem] - {message}");
+        Debug.Log($"[BetterTotem] - {message}");
+    }
+    private static void BetterLog(object message)
+    {
+        BetterLog($"{message}");
+    }
+}
